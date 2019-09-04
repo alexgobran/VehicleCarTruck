b90d74f5895050ec3b2c84701e95fec68cc37b2c
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-09-04 12:05:03 -0400

    initial commit

--- VehicleCarandTruck/Program.cs
+++ VehicleCarandTruck/Program.cs
@@ -5,7 +5,8 @@ using VehicleCarTruckLib;
 namespace VehicleCarandTruck {
     class Program {
 
-
+        //instead of creating a type of vehicle ( car, truck) in the vehicle class. Creating a new Class called Car, and another Class called Truck.
+        //opposed to the alternative of have all in the vehicle class by using  string Type;
 
         static void Main(string[] args) {
             (new Program()).Run();
